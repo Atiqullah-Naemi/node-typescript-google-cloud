da1c6fdccf00a7a1bc9e96108cc3019a97e560df
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2021-03-13 11:18:10 +1100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import { Firestore } from "@google-cloud/firestore";
+
+const app = express();
+const PORT = 5000;
+
+// Create a new client
+const db = new Firestore();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({ message: "working" });
+});
+
+app.get("/:breed", async (req: Request, res: Response) => {
+  const breed = req.params.breed;
+
+  const query = db.collection("dogs").where("name", "==", breed);
+  const querySnapshot = await query.get();
+
+  if (querySnapshot.size > 0) {
+    res.status(200).send({ data: querySnapshot.docs[0].data() });
+  } else {
+    res.status(200).send({ message: "Not Found" });
+  }
+});
+
+app.listen(PORT, () => console.log(`app running on ${PORT}`));
