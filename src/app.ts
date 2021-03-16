7e18c7b256b35e78b2ac8ddbd69bb4d27a7cb204
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2021-03-16 20:48:16 +1100

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,61 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, NextFunction, Response } from "express";
 import { Firestore } from "@google-cloud/firestore";
+import Multer from "multer";
+import { format } from "util";
+import { Storage } from "@google-cloud/storage";
 
 const app: Application = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
 // Create a new client
 const db = new Firestore();
+
+// Creates a client
+const storage = new Storage();
+const bucket = storage.bucket(<string>process.env.CLOUD_BUCKET_ID);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({ message: "working app" });
 });
+
+app.post(
+  "/upload",
+  multer.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!req.file) {
+      res.status(400).send("No file uploaded.");
+      return;
+    }
+
+    const blob = bucket.file(req.file.originalname);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+
+    blobStream.on("error", (err) => {
+      next(err);
+    });
+
+    blobStream.on("finish", () => {
+      const publicUrl = format(
+        `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+      );
+
+      return res.status(200).send(publicUrl);
+    });
+
+    blobStream.end(req.file.buffer);
+  }
+);
 
 app.get("/:breed", async (req: Request, res: Response) => {
   const breed = req.params.breed;
@@ -25,7 +70,7 @@ app.get("/:breed", async (req: Request, res: Response) => {
   }
 });
 
-app.post("/", async (req, res) => {
+app.post("/", async (req: Request, res: Response) => {
   const data = {
     name: req.body.name,
     origin: req.body.origin,
