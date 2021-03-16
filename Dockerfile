1eacee533d58433351aa8958d0b060823829d17f
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2021-03-16 21:27:59 +1100

    updated again

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,6 @@ COPY .env ./dist
 
 WORKDIR ./dist
 
-EXPOSE 3000
+EXPOSE 8080
 
 CMD node src/app.js
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import { Firestore } from "@google-cloud/firestore";
 import Multer from "multer";
 import { format } from "util";
 import { Storage } from "@google-cloud/storage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 8080;
