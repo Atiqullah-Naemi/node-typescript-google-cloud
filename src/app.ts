a50c2e541ae2759abea579c69b998b552a381f01
atiqullah-naemi <atiqullah.naemi21@gmail.com>
2021-04-21 22:38:46 +1000

    cloud_key.json initiated

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import Multer from "multer";
 import { format } from "util";
 import { Storage } from "@google-cloud/storage";
 import * as dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
@@ -20,11 +21,40 @@ const multer = Multer({
 });
 
 // Create a new client
-const db = new Firestore();
+const db = new Firestore({
+  projectId: "gcloud-practice-303820",
+  keyFilename: path.join(__dirname, "./cloud_key.json"),
+});
 
 // Creates a client
-const storage = new Storage();
+const storage = new Storage({
+  keyFilename: path.join(__dirname, "./cloud_key.json"),
+});
 const bucket = storage.bucket(<string>process.env.CLOUD_BUCKET_ID);
+
+const imageUpload = (file: any) =>
+  new Promise((resolve, reject) => {
+    const { originalname, buffer } = file;
+
+    const blob = bucket.file(originalname);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+
+    blobStream.on("error", (err) => {
+      reject(err);
+    });
+
+    blobStream.on("finish", () => {
+      const publicUrl = format(
+        `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+      );
+
+      resolve(publicUrl);
+    });
+
+    blobStream.end(buffer);
+  });
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({ message: "working app..." });
@@ -33,30 +63,27 @@ app.get("/", (req: Request, res: Response) => {
 app.post(
   "/upload",
   multer.single("file"),
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     if (!req.file) {
       res.status(400).send("No file uploaded.");
       return;
     }
 
-    const blob = bucket.file(req.file.originalname);
-    const blobStream = blob.createWriteStream({
-      resumable: false,
-    });
+    const imageUrl = await imageUpload(req.file);
 
-    blobStream.on("error", (err) => {
-      next(err);
-    });
+    console.log({ imageUrl });
 
-    blobStream.on("finish", () => {
-      const publicUrl = format(
-        `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-      );
+    const data = {
+      name: req.body.name,
+      origin: req.body.origin,
+      lifeExpectacy: req.body.lifeExpectacy,
+      type: req.body.type,
+      imageUrl,
+    };
 
-      return res.status(200).send(publicUrl);
-    });
+    await db.collection("dogs").doc().set(data);
 
-    blobStream.end(req.file.buffer);
+    res.status(200).send({ success: true, data: { dogs: data } });
   }
 );
 
@@ -74,6 +101,8 @@ app.get("/:breed", async (req: Request, res: Response) => {
 });
 
 app.post("/", async (req: Request, res: Response) => {
+  console.log({ req });
+
   const data = {
     name: req.body.name,
     origin: req.body.origin,
